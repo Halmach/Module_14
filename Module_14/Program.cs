f1436b54d61854037cd03228ffb18be33fa32f1e
Khalmatov <large000@yandex.ru>
2021-12-25 15:24:22 +0300

    Добавьте файлы проекта.

---
+++ Module_14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Module_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objects = new List<object>()
+            {
+               1,
+               "Сергей ",
+               "Андрей ",
+               300,
+            };
+
+            var names = from n in objects where n is string orderby n select n;
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names = objects.Where(n => n is string).OrderBy(n => n);
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
