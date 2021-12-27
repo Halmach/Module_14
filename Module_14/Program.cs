e59131790742df9b9e8a93623423f0fd839ddbde
Khalmatov <large000@yandex.ru>
2021-12-27 17:44:49 +0300

    Задание 14.2.3 ShowHowLetWorkingInLINQ

--- Module_14/Program.cs
+++ Module_14/Program.cs
@@ -8,7 +8,34 @@ namespace Module_14
     {
         static void Main(string[] args)
         {
-            ChooseByNameLengthWithOrdering();
+            ShowHowLetWorkingInLINQ();
+
+        }
+
+        private static void ShowHowLetWorkingInLINQ()
+        {
+            // Наш список студентов
+            List<Student> students = new List<Student>
+{
+               new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+               new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+               new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
+               new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+            };
+
+            var studentsFilter = from s in students
+                                 where s.Age < 27
+                                 let YearOfBirth = DateTime.Now.Year - s.Age
+                                 select new Application
+                                 {
+                                     Name = s.Name,
+                                     YearOfBirth = YearOfBirth
+                                 };
+            foreach (var stud in studentsFilter)
+            {
+                Console.WriteLine(stud.Name + " " + stud.YearOfBirth);
+            }
+
 
         }
         
---
+++ Module_14/Student.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Module_14
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Languages { get; set; }
+    }
+}
